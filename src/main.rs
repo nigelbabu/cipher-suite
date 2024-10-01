87545b0b0821eeb57858adf19289f4e7eca49087
Nigel Babu <nigel@nigelb.me>
2024-10-01 21:22:05 +0100

    Fix up the commandline parsing

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,20 @@ use clap::{Parser, Subcommand};
 #[command[version, about, long_about = None]]
 struct Args {
     /// The key to use
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t=String::from("secret"))]
     key: String,
+
+    /// The length of the shift
+    #[arg(short, long, default_value_t=3)]
+    shift: u8,
 
     /// The filename to encrypt
     #[arg(short, long)]
     filename: String,
+
+    /// The filename to encrypt
+    #[arg(short, long)]
+    cipher: String,
 
     #[command(subcommand)]
     command: Option<Commands>,
@@ -31,10 +39,24 @@ fn main() -> std::io::Result<()> {
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
+    let cipher = &args.cipher.to_lowercase();
     
     let result = match &args.command {
-        Some(Commands::Encrypt {}) => cipher_suite::vignere::encrypt(&contents, args.key.as_str()),
-        Some(Commands::Decrypt {}) => cipher_suite::vignere::decrypt(&contents, args.key.as_str()),
+        Some(Commands::Encrypt {}) => {
+            match cipher.as_str() {
+                "caesar" => cipher_suite::caesar::encrypt(&contents, args.shift),
+                "vignere" => cipher_suite::vignere::encrypt(&contents, args.key.as_str()),
+                _ => Err(String::from("Cipher not found"))
+            }
+
+        }
+        Some(Commands::Decrypt {}) => {
+            match cipher.as_str() {
+                "caesar" => cipher_suite::caesar::decrypt(&contents, args.shift),
+                "vignere" => cipher_suite::vignere::decrypt(&contents, args.key.as_str()),
+                _ => Err(String::from("Cipher not found"))
+            }
+        }
         None => Err(String::from("Command not provided: encrypt or decrypt")),
     };
     match result {
